c3172a23d7d3474fc34f1a1bdad629ac4c74c87b
Philip Mallory <philip@pmallory.com>
2012-04-06 17:47:20 -0400

    improve comments

--- life.py
+++ life.py
@@ -4,11 +4,19 @@ import os
 import sys
 import time
 
+# initial board configuration. The board is a dictionary mapping
+# cell coords=>cell state. Dead cells can have a value of False, or they can
+# can be left out entirely
 gen1 = {(31,32):True, (31,33):True, (32,31):True, (32,32):True, (33,32):True}
 
-board_size = 60
-
 def iterate(board, board_size):
+    """Given a board state generate the next generation and return it.
+
+    board: a dictionary representing a genration's state
+    board_size: how wide the (square) board is. Cells beyond this are
+                effectively dead.
+
+    """
     new_board = {}
 
     for cell in itertools.product(range(board_size), repeat=2):
@@ -43,12 +51,17 @@ def iterate(board, board_size):
 
 def main(stdscr):
     current_gen = gen1
+
+    # hide the cursor
     curses.curs_set(0)
 
+    # set the board size such that it fits within the terminal window
     board_size = min(stdscr.getmaxyx())
 
     while True:
-        for (i, cell_coords) in enumerate(itertools.product(range(board_size), repeat=2)):
+        # draw living cells
+        # TODO this can by way more efficient.
+        for cell_coords in itertools.product(range(board_size), repeat=2):
             if current_gen.get(cell_coords):
                 stdscr.addch(cell_coords[0], cell_coords[1], 'X')
         stdscr.refresh()
