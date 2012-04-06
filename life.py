e2694df88ac624b4df4b538918cf0cc3004486a9
Philip Mallory <philip@pmallory.com>
2012-04-06 14:01:49 -0400

    Set board size based on size of terminal.

--- life.py
+++ life.py
@@ -8,7 +8,7 @@ gen1 = {(21,22):True, (21,23):True, (22,21):True, (22,22):True, (23,22):True}
 
 board_size = 60
 
-def iterate(board):
+def iterate(board, board_size):
     new_board = {}
 
     for cell in itertools.product(range(board_size), repeat=2):
@@ -39,8 +39,6 @@ def iterate(board):
                 new_board[cell] = False
         elif neighbour_count is 3:
             new_board[cell] = True
-        else:
-            new_board[cell] = False
 
     return new_board
 
@@ -49,15 +47,17 @@ def main(stdscr):
     current_gen = gen1
     curses.curs_set(0)
 
+    board_size = min(stdscr.getmaxyx())
+
     while True:
         for (i, cell_coords) in enumerate(itertools.product(range(board_size), repeat=2)):
             if current_gen.get(cell_coords):
                 stdscr.addch(cell_coords[0], cell_coords[1], 'X')
         stdscr.refresh()
 
-        time.sleep(0.1)
+        time.sleep(0.01)
         stdscr.erase()
-        current_gen = iterate(current_gen)
+        current_gen = iterate(current_gen, board_size)
 
 if __name__=='__main__':
     curses.wrapper(main)
