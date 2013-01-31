f5afd3105fc3dada41449e8f06ed3b753caad703
pmallory <philip@pmallory.com>
2013-01-30 23:01:03 -0500

    even more itertools!

--- life.py
+++ life.py
@@ -1,56 +1,38 @@
 import curses
-import itertools
+from itertools import product, ifilter, islice, cycle
 import time
 
-# initial board configuration. A board's state is represented by the set
-# of coordinates of living cells.
-gen1 = set([(31, 32), (31, 33), (32, 31), (32, 32), (33, 32)])
+# initial board configuration.
+#A board's state is represented by the set  of coordinates of living cells.
+gen1 = set([(11, 32), (11, 33), (12, 31), (12, 32), (13, 32)])
+
+# cycling iterator of a cells eight neigbors
+neighbors = cycle(ifilter(lambda x: x!=(0,0), product(xrange(-1,2), repeat=2)))
 
 def iterate(board, board_size):
     """Given a board state generate the next generation and return it.
 
-    board: a set representing a genration's state
-    board_size: how wide the (square) board is. Cells beyond this are
+    board: a set of tuples representing living cells.
+    board_size: The dimesnions of the world. Cells beyond this are
                 effectively dead.
-
     """
     new_board = set()
 
-    for cell in rectangular_coordinates(board_size):
-        # count cell's neigbors
-        neighbour_count = 0
-        if (cell[0]-1, cell[1]-1) in board:
-            neighbour_count += 1
-        if (cell[0]-1, cell[1]) in board:
-            neighbour_count += 1
-        if (cell[0]-1, cell[1]+1) in board:
-            neighbour_count += 1
-        if (cell[0], cell[1]-1) in board:
-            neighbour_count += 1
-        if (cell[0], cell[1]+1) in board:
-            neighbour_count += 1
-        if (cell[0]+1, cell[1]-1) in board:
-            neighbour_count += 1
-        if (cell[0]+1, cell[1]) in board:
-            neighbour_count += 1
-        if (cell[0]+1, cell[1]+1) in board:
-            neighbour_count += 1
+    for cell in product(xrange(board_size[0]), xrange(board_size[1])):
+        # count cell's living neighbors
+        neighbor_count = 0
+        for neighbor in islice(neighbors, 8):
+            if (cell[0]+neighbor[0], cell[1]+neighbor[1]) in board:
+                neighbor_count += 1
 
         # determine which cells will be alive on the next generation
         if cell in board:
-            if 2 <= neighbour_count <= 3:
+            if 2 <= neighbor_count <= 3:
                 new_board.add(cell)
-        elif neighbour_count is 3:
+        elif neighbor_count is 3:
             new_board.add(cell)
 
     return new_board
-
-
-def rectangular_coordinates(dimension):
-    for i in range(dimension[0]):
-        for j in range(dimension[1]):
-            yield (i, j)
-
 
 def main(stdscr):
     current_gen = gen1
@@ -61,11 +43,17 @@ def main(stdscr):
     # hide the cursor
     curses.curs_set(0)
 
-    # set the board size such that it fits within the terminal window
-    board_size = stdscr.getmaxyx()
-
     while True:
-        # draw living cells
+        # set the board size such that it fits within the terminal window
+        board_size = stdscr.getmaxyx()
+
+        # generate the next time step from the current
+        current_gen = iterate(current_gen, board_size)
+
+        # erase the screen
+        stdscr.erase()
+
+        # draw the new generation of cells
         for cell_coords in current_gen:
             stdscr.addch(cell_coords[0], cell_coords[1], 'O')
         stdscr.refresh()
@@ -74,9 +62,8 @@ def main(stdscr):
         if stdscr.getch() == ord('q'):
             break
 
+        # slow the animation
         time.sleep(0.1)
-        stdscr.erase()
-        current_gen = iterate(current_gen, board_size)
 
 if __name__=='__main__':
     curses.wrapper(main)
